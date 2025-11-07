4f966d512047c6b05fdeb79502b8beca645c34e6
Stefan Kasabov <114361464+H3roOn30FPS@users.noreply.github.com>
2025-11-07 11:55:53 +0200

    Added via Upload

---
+++ First_Year/07.11.2025 - week 7 - Lab Section/New elements in between lists.py
@@ -0,0 +1,27 @@
+#Ex. 2: User inputs a number s (3 > s < 6). then inputs s amount of numbers. The program creates one list with s amount of
+#elements (example:s = 3; [21, 5, 17]). After that this list adds the sum of two adjacent numbers 
+# (l1 = [21, 5, 17] = > [21, 26, 5, 22, 17]) (26 is sum of 21 and 5, and 22 is sum of 5 and 17.)
+
+newn = None
+while True:
+    n = float(input("Input a real number (between 3 and 6): "))
+    if n >= 3 and n <= 6:
+        newn = int(n)
+        break
+    else:
+        print("Error... Please enter a number between 3 and 6!\n")
+
+numbers = []
+for i in range(newn):
+    num = int(input(f"Enter number {i+1}: "))
+    numbers.append(num)
+
+new_list = []
+for i in range(len(numbers) - 1):
+    new_list.append(numbers[i])
+    new_list.append(numbers[i] + numbers[i + 1])
+
+new_list.append(numbers[-1])
+
+print("Original list:", numbers)
+print("New list:", new_list)
