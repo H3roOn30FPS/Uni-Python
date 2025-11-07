90db327d77103ca80f03e7ec5e9439b087ba2ecb
Stefan Kasabov <114361464+H3roOn30FPS@users.noreply.github.com>
2025-11-07 12:01:50 +0200

    Delete First_Year/31.10.2025 - week 7 - Lab Section directory

--- First_Year/31.10.2025 - week 7 - Lab Section/User inputs 5 random numbers.py
+++
@@ -1,7 +0,0 @@
-#User inputs 5 random numbers for the keyboard. Write a program that will: find the sum of those numbers
-
-sumall = 0
-for n in range (5):
-    number = float(input("Enter a number: "))
-    sumall += number
-print(sumall)
