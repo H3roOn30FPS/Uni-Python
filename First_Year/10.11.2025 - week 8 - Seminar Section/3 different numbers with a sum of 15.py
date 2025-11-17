82aabaa7f522c948abca534199a583478679da89
Stefan Kasabov <114361464+H3roOn30FPS@users.noreply.github.com>
2025-11-17 14:25:18 +0200

    added comment for consecutive numbers
    By changing the range from (1, 10) to (a + 1, 10), the programme checks for the sum of 15 by 3 consecutive numbers.

--- First_Year/10.11.2025 - week 8 - Seminar Section/3 different numbers with a sum of 15.py
+++ First_Year/10.11.2025 - week 8 - Seminar Section/3 different numbers with a sum of 15.py
@@ -1,7 +1,7 @@
 #Задача 13. Направете програма, която намира всички комбинации от 3 различни числа в интервала от 1 до 9, чиято сума е 15.
 for a in range(1, 10):
-    for b in range(1, 10):
-        for c in range(1, 10):
+    for b in range(1, 10):    #alternatively range(a + 1, 10)        | this will print only
+        for c in range(1, 10):    #alternatively range (b + 1, 10)   | the consecutive numbers which = 15
             if a != b and a != c and b != c:
                 if a + b + c == 15:          
                     print(a, b, c)
